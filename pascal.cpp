9dcbef4d013582cbb87da27f27578dd9fa4f9383
Adithya Damodara <adithyadamodar90@gmail.com>
2026-01-22 13:06:43 +0530

    Pascal trianlge single line

--- pascal.cpp
+++ pascal.cpp
@@ -3,9 +3,12 @@ using namespace std;
 
 int main(){
     int n = 5;
-        
-    for(int i=2;i<n;i++);
-
-
+    int ans=1;
+    
+    // Prints single line
+    for(int i=0;i<=n;i++){
+        cout<<ans<<" ";
+        ans = ans * (n-i) / (i+1);
+    }
     return 0;
 }
