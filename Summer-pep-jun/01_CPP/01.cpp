0aafef0362af577934da9f1c45a1310ff12fc176
Adithya Damodara <adithyadamodar90@gmail.com>
2026-06-22 11:37:13 +0530

    Updated structure

---
+++ Summer-pep-jun/01_CPP/01.cpp
@@ -0,0 +1,45 @@
+// Data Types Variables
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int -> 4 byte
+    // long long -> 8 byte
+    // float => 4 byte
+    // double -> 8 byte
+    // char -> 1 byte
+    // bool -> 1 byte
+    
+    // int age = 20;
+    // int no = 15;
+    // if(age >= 18 && no > 10){
+    //     cout<<"Test is conducted"<<endl;    
+    // } 
+    // if(age > 18 && no < 18){
+    //     cout<<"Hello"<<endl;
+    // } 
+    // else {
+    //     cout<<"No test conducted"<<endl;
+    // }
+
+    // char grade = 'A';
+    // switch(grade){
+    //     case 'A':
+    //         cout<<"First grade"<<endl;
+    //         break;
+    //     case 'B':
+    //         cout<<"Second grade"<<endl;
+    //         break;
+    //     default:
+    //         cout<<"None";
+    // }
+
+
+    // int x = 30, y = 20; 
+    // cout<<(x > y ? x:y); 
+    
+    
+    return 0;
+}
