f8ed0dc6a76aafe7e4833577cb56d045bcf96168
Adithya Damodara <adithyadamodar90@gmail.com>
2026-06-30 11:58:26 +0530

    additional code

--- Summer-pep-jun/01_CPP/09.cpp
+++ Summer-pep-jun/01_CPP/09.cpp
@@ -4,14 +4,22 @@
 using namespace std;
 
 
-
-
-
 int main(){
-    int n;
-    cout<<"Enter n: ";
-    cin>>n;
+    int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j+=2){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
     return 0;
 }
-
